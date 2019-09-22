e1e23a48551bbd58528b8365707595338f6ff292
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2019-09-22 18:38:58 +0200

    Finished tutorial

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+      
+    // Update is called once per frame
+    void Update(){}
+
+    //Fixed Update is called just before performing any physics calculations
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+
+        rb.AddForce(movement*speed);
+
+    
+    }
+
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 8)
+        {
+            winText.text = "YOU WIN!";
+        }
+    }
+
+   
+}
+
